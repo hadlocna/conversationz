ef25756514c956c9f4af1222ca57f22ae58f9c08
Nathan Hadlock <hadlocna@gmail.com>
2013-12-19 12:37:12 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Conversationz::Application.config.secret_key_base = 'a062c2cbc66e386c47f84d439e13f6b387dae217a9b945d0f44165ee1c4a68a9942323652922fa21a10e533cd1ade9b2359b7812b16d7be8c98380bf247c26ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Conversationz::Application.config.session_store :cookie_store, key: '_conversationz_session'
