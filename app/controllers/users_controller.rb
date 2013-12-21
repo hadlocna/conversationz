417ac07a85fbbabb4337f7773beb8a487ce953f1
Nathan Hadlock <hadlocna@gmail.com>
2013-12-21 14:15:51 -0500

    basic navigation and header/footer style added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,45 @@
 class UsersController < ApplicationController
 
-def index
+  def new
   end
 
   def create
+
+    @user = User.find_or_create_by_email(params[:user])
+    if @user.valid?
+      current_user_assign(@user)
+      redirect_to "/"
+    else
+      redirect_to "/users/new"
+    end
+
   end
 
-  def new
+  def create_login
+    @user = User.find_by_email(params[:user][:email])
+
+    if @user
+      if @user.authenticate(params[:user][:password])
+        current_user_assign(@user)
+
+        redirect_to "/"
+      end
+
+    else
+      redirect_to :action => 'login'
+    end
+  end
+
+  def login
+  end
+
+  def index
+
+  end
+
+  def logout
+    logout_user
+    redirect_to "/"
   end
 
 end
